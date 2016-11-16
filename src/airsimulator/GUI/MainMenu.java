7eaf14af2e74596c2fc51ddea7b82bc3b3da0e18
Adam <Adam@My-PC>
2016-11-16 22:00:59 +0100

    vytvoreni uvodniho menu

---
+++ src/airsimulator/GUI/MainMenu.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airsimulator.GUI;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Locale;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Adam
+ */
+public class MainMenu extends JFrame{
+    
+    GridBagConstraints gridBagConstraints;
+    GridBagConstraints mainBagConstraints;
+            
+    JPanel mainPanel;
+    
+    JButton spustit_aplikaci;
+    JButton napoveda;
+    JButton ukoncit;
+    
+    public MainMenu(){
+        super("Úvodní nabídka");
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        super.setLayout(new GridBagLayout());
+        
+        this.mainPanel = new JPanel();
+        this.mainPanel.setLayout(new GridBagLayout());
+        
+        this.gridBagConstraints = new GridBagConstraints();
+        this.gridBagConstraints.insets = new Insets(5, 0, 5, 0);
+        this.gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        this.gridBagConstraints.fill = GridBagConstraints.BOTH;
+        
+        this.spustit_aplikaci = new JButton("Spustit aplikaci");
+        this.spustit_aplikaci.addActionListener(new PlaySimulation());
+        this.napoveda = new JButton("Nápověda");
+        this.napoveda.addActionListener(new ViewHelp());
+        this.ukoncit = new JButton("Ukončit");
+        this.ukoncit.addActionListener(new Exit());
+        
+        this.mainPanel.add(this.spustit_aplikaci, this.gridBagConstraints);
+        this.mainPanel.add(this.napoveda, this.gridBagConstraints);
+        this.mainPanel.add(this.ukoncit, this.gridBagConstraints);
+        
+        this.mainBagConstraints = new GridBagConstraints();
+        this.mainBagConstraints.anchor = GridBagConstraints.CENTER;
+        super.add(this.mainPanel, this.mainBagConstraints);
+                
+        super.setSize(500, 500);
+        super.setLocation(dim.width/2-super.getSize().width/2, dim.height/2-super.getSize().height/2);
+
+        super.setVisible(true);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private class ViewHelp implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            HelpWindow helpWindow = new HelpWindow();
+        }
+
+    }
+
+    private class Exit implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Object[] opt = {"Ano","Ne"};
+            int answer = JOptionPane.showOptionDialog(null,"Opravdu chcete ukončit aplikaci?", 
+                    "Ukončení aplikace",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE, null,opt, opt[0]);
+            if( answer == 0){
+                System.exit(1);
+            }
+        }
+
+    }
+
+    private class PlaySimulation implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            PlayWindow playWindow = new PlayWindow();
+            dispose();
+        }
+    }
+    
+}
